af722df6b777ad182567ea44c6cde9688f7a73b3
Rodolfo Silva <contato@rodolfosilva.com>
2017-03-12 22:52:25 -0300

    Fix testes

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,13 +1,13 @@
 import { Http } from '@angular/http';
 import { Restangular } from './restangular';
 
-export function RestangularFactory(http: Http, factory?: Function, ...deps: Array<any>): Restangular {
+export function RestangularFactory(http: Http, customFactory?: Function, ...deps: Array<any>): Restangular {
   const restangular: Restangular = new Restangular(http);
 
   deps = [].concat(deps);
 
-  if (factory) {
-    factory(restangular.config, ...deps);
+  if (customFactory) {
+    customFactory(restangular.config, ...deps);
   }
 
   return restangular;
